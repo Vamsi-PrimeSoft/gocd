fc4cb601d6d3e42bcbe749ff948ad2c133ab0c7c
Vamsi-PrimeSoft <133194079+Vamsi-PrimeSoft@users.noreply.github.com>
2024-12-27 12:59:49 +0530

    Update folder_structure.sh

--- folder_structure.sh
+++ folder_structure.sh
@@ -11,7 +11,8 @@ check_and_create_folder() {
 
   if [ ! -d "$folder_path" ]; then
     mkdir "$folder_path"
-    echo "Created folder '$folder_path'"
+    chown ubuntu:ubuntu "$folder_path"
+    echo "Created folder '$folder_path' and set ownership to 'ubuntu:ubuntu'"
   else
     echo "Folder '$folder_path' already exists"
   fi
@@ -20,8 +21,8 @@ check_and_create_folder() {
 # Check and create the "archive" folder
 check_and_create_folder "$archive_folder"
 
-# Check and create the "prime-square" folders
+# Check and create the "prime-square" folder
 check_and_create_folder "$primesquare_folder"
 
-#Check and Create the "Roll-back" folder
+# Check and create the "rollback" folder
 check_and_create_folder "$rollback_folder"
